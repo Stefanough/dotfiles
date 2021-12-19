6dddb55711359a96562328241966e892f7f4980d
Stefan Armijo <stefan.armijo@gmail.com>
2021-12-18 21:08:13 -0500

    Check for Brew installation.

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 # Install HomeBrew and listed packages.
 
+# Check if Brew is installed. If it is, exit with 0. The command builtin runs
+# commands. With the -v flag it writes the pathname or name of the command that
+# would be executed. 
+if command -v brew &> /dev/null; then
+  echo "Brew appears to be installed already."
+  exit 0
+fi
+
 echo 'Installing Homebrew...'
 
 echo 'Downloading Homebrew installer...'
@@ -12,4 +20,5 @@ echo 'Downloading Homebrew installer...'
 # -S show error. When used with -s will still show errors.
 # -o output to file instead of stdout
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-run brew bundle --verbose --no-lock --file="packages.personal.Brewfile"
+
+brew bundle --verbose --no-lock --file="packages.personal.Brewfile"

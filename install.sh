2f20d4909cec67206172a34517094bda7716800d
Stefan Armijo <stefan.armijo@gmail.com>
2021-12-19 02:23:48 -0500

    Update install script to allow for dryruns.

--- install.sh
+++ install.sh
@@ -1,33 +1,54 @@
 #!/bin/sh
 
-# Install HomeBrew and listed packages.
-
+# Install HomeBrew and packages.
+#
+# options:
+#   -d dry run
+#
 # TODO:
 # set faster scroll speed for mac if possible
 # set default shell to bash
-# arg for dryrun
 
-# Check if Brew is installed. If it is, exit with 0.
+# set variable for dry run
+D='false'
+while getopts 'd' flag; do
+  case "${flag}" in
+    d) D='true' ;;
+    *) echo "Unexpected option ${flag}" >&2; exit 1 ;;
+  esac
+done
+readonly D
+
+# Check if Brew is installed.
+#
 # The command builtin runs commands.
 # flags:
-#   -v writes the pathname or name of the command that would be executed. 
+#   -v writes the pathname or name of the command that would be executed.
 if command -v brew &> /dev/null; then
-  echo "Brew appears to be installed already."
-  exit 0
+  echo "Brew appears to be already installed."
+else
+  echo 'Installing Homebrew...'
+  echo 'Downloading Homebrew installer...'
+  if [ "$D" = 'true' ]; then
+    echo 'Dry run, skipping brew install script'
+  else
+    # curl flags:
+    #   -f fail silently,
+    #   -L redo request with returned location (server indicates that requested
+    #   resource has moved
+    #   -s silent. Don't show progress meters
+    #   -S show error. When used with -s will still show errors.
+    #   -o output to file instead of stdout
+    # bash flags:
+    #   -c read command from the given string
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
 fi
 
-echo 'Installing Homebrew...'
-
-echo 'Downloading Homebrew installer...'
-# curl flags:
-#   -f fail silently,
-#   -L redo request with returned location (server indicates that requested
-#   resource has moved
-#   -s silent. Don't show progress meters
-#   -S show error. When used with -s will still show errors.
-#   -o output to file instead of stdout
-# bash flags:
-#   -c read command from the given string
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew bundle --verbose --no-lock --file="packages.personal.Brewfile"
+# Install packages listed in the provided Brewfile.
+echo 'installing packages using brew bundle...'
+if [ "$D" = 'true' ]; then
+  echo 'Dry run, skipping install of packages.'
+else
+  brew bundle --verbose --no-lock --file="packages.personal.Brewfile"
+fi
